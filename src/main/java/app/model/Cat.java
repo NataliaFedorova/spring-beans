d47d2888922f72b0d57675611666059d9b34c6df
Natalia Fedorova <natalia.fedorova.92@yandex.ru>
2021-07-20 06:51:35 +0300

    2.1.3

--- src/main/java/app/model/Cat.java
+++ src/main/java/app/model/Cat.java
@@ -2,10 +2,11 @@ package app.model;
 
 import org.springframework.stereotype.Component;
 
-@Component
+@Component("cat")
 public class Cat extends Animal {
     @Override
     public String toString() {
         return "Im a Cat";
     }
 }
+
--- src/main/java/app/model/Dog.java
+++ src/main/java/app/model/Dog.java
@@ -3,6 +3,10 @@ package app.model;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-@Component
-public abstract class Animal {
+@Component ("dog")
+public class Dog extends Animal{
+    @Override
+    public String toString() {
+        return "Im a Dog";
+    }
 }
